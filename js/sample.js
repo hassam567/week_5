d5f4047345ed24ff602ed340ab09c180e35c29d1
Hassam Tariq <151511092+hassam567@users.noreply.github.com>
2023-12-28 16:24:43 +0500

    second commit

--- js/sample.js
+++ js/sample.js
@@ -1,76 +1,127 @@
 function getValue() {
-    var inputField = document.getElementById("inputValue");
-    var userName = inputField.value;
-    var errorContainer = document.getElementById("errorContainer");
-
-    fetch("https://api.github.com/users/" + userName)
-        .then((response) => {
-          if (!response.ok) {
-          
-            throw new Error("Enter valid userName");
-        }
-            return response.json(); 
-        })
-        .then((value) => {
-          errorContainer.textContent = "";
-           var imageUrl= document.getElementById("userImage")
-           if(value.name)
-           {
-            var nameheading=document.getElementById("nameHeading");
-            nameheading.innerHTML="Name"
-            var name=document.getElementById("yourName");
-            name.innerHTML=value.name;
-           }
-           if(value.url)
-           {
-            var url=document.getElementById("yourURL");
-         
-            var nameheading=document.getElementById("urlHeading").textContent="URL";
-            var link=document.createElement("a");
-            link.href=value.url;
-            link.target="-blank";
-            link.textContent="This is my gitHub URL:  "+value.url;
-            url.appendChild(link);
-
-         
-           
-           }
-           if(value.company)
-           {
-            var nameheading=document.getElementById("companyHeading");
-            nameheading.innerHTML="Company"
-            var name=document.getElementById("companyName");
-            name.innerHTML=value.company;
-           }
-           
-
-           if(value.bio)
-           {
-            var bioId=document.getElementById("bioHeading")
-            bioId.innerHTML="Bio"
-            var bio_info= document.getElementById("bio")
-            bio_info.innerHTML=value.bio
-
-           }
-          
-           console.log(value)
+  var inputField = document.getElementById("inputValue");
+  var userName = inputField.value;
+  var errorContainer = document.getElementById("errorContainer");
+  var userCard = document.getElementById("userCard");
 
 
-           if (value.avatar_url) {
-            // If there is an image source, set border-radius to 50%
-            imageUrl.src = value.avatar_url;
-            imageUrl.style.borderRadius = "50%";
-            imageUrl.style.width = "100px";
-            imageUrl.style.height = "100px"; // Or your desired border-radius
-          } else {
-            // If there is no image source, set border-radius to 0
-            imageUrl.style.borderRadius = "0%";
-          }
-          inputField.value = "";
-         
-        })
-        .catch((error) => {
-            // console.error("Error fetching GitHub data:", error);
-            errorContainer.textContent = "Error: " + error.message;
-        });
+  fetch("https://api.github.com/users/" + userName)
+    .then((response) => {
+      if (!response.ok) {
+
+        throw new Error("Enter valid username");
+      }
+      return response.json();
+    })
+    .then((value) => {
+      errorContainer.textContent = "";
+      var imageUrl = document.getElementById("userImage")
+      if (value.name) {
+        var nameheading = document.getElementById("nameHeading");
+        nameheading.innerHTML = "Name"
+        var name = document.getElementById("yourName");
+        name.innerHTML = value.name;
+      }
+      else {
+
+        var nameheading = document.getElementById("nameHeading");
+        nameheading.innerHTML = "Name";
+        var name = document.getElementById("yourName");
+        name.innerHTML = "Not found";
+      }
+
+      var lineBreak = document.createElement("br");
+      name.appendChild(lineBreak);
+      if (value.company) {
+        var nameheading = document.getElementById("companyHeading");
+        nameheading.innerHTML = "Company"
+        var name = document.getElementById("companyName");
+        name.innerHTML = value.company;
+      }
+      else {
+
+        var nameheading = document.getElementById("companyHeading");
+        nameheading.innerHTML = "Company";
+        var name = document.getElementById("companyName");
+        name.innerHTML = "Not Found";
+      }
+
+
+      if (value.html_url) {
+        var url = document.getElementById("yourURL");
+
+
+        url.innerHTML = "";
+
+        var urlHeading = document.getElementById("urlHeading").textContent = "URL";
+        var link = document.createElement("a");
+        link.href = value.html_url;
+        link.target = "-blank";
+        link.textContent = "This is my GitHub URL:  " + value.html_url;
+        url.appendChild(link);
+      } else {
+        // Display message if URL is null
+        var url = document.getElementById("yourURL");
+        var urlHeading = document.getElementById("urlHeading");
+        urlHeading.textContent = "URL";
+        url.innerHTML = "Not Found";
+      }
+
+
+
+
+
+
+      if (value.bio) {
+        var bioId = document.getElementById("bioHeading")
+        bioId.innerHTML = "Bio"
+        var bio_info = document.getElementById("bio")
+        bio_info.innerHTML = value.bio
+
+      }
+
+      else {
+
+        var bioId = document.getElementById("bioHeading");
+        var bio_info = document.getElementById("bio")
+        bio_info.textContent = "Bio";
+        bio_info.innerHTML = "Not Found";
+        bio_info.style.color = "orange"
+        bio_info.style.fontSize = "20px"
+        bio_info.style.fontWeight = "bold"
+        bio_info.style.textAlign = "Center"
+
+      }
+
+      console.log(value)
+
+
+      if (value.avatar_url) {
+        // If there is an image source, set border-radius to 50%
+        imageUrl.src = value.avatar_url;
+        imageUrl.style.borderRadius = "50%";
+        imageUrl.style.width = "100px";
+        imageUrl.style.height = "100px"; // Or your desired border-radius
+      } else {
+        // If there is no image source, set border-radius to 0
+        imageUrl.style.borderRadius = "0%";
+      }
+      userCard.style.display = "block";
+      inputField.value = "";
+
+    })
+    .catch((error) => {
+      // console.error("Error fetching GitHub data:", error);
+      errorContainer.textContent = "Error: " + error.message;
+      userCard.style.display = "none";
+    });
 }
+
+// Add event listener for Enter key press
+var inputField = document.getElementById("inputValue");
+inputField.addEventListener("keyup", function (event) {
+  if (event.key === "Enter") {
+    getValue();
+  }
+});
+
